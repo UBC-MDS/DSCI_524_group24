d0e6be9b3aa2d18a94e19c22f79dde74384119bd
Suryash Chakravarty <suryash.c@gmail.com>
2026-01-14 16:22:20 -0800

    created basic template for tes_get_line

--- tests/unit/test_get_line.py
+++ tests/unit/test_get_line.py
@@ -1,7 +1,10 @@
 """
-A test module that tests the ridge_r2 module.
+A test module that tests the get_reg_line module.
 
 This file contains tests which are used to test the output of the function
-ridge_get_r2(), in the ridge_r2 module. The tests can be run by calling 
+get_reg_line(), in the get_reg_line module. The tests can be run by calling 
 pytest -v . in the root directory
 """
+
+from ridge_remake.get_reg_line import get_reg_line
+
