379ea7058edb703ce8579ecd320da2fd466d6611
Suryash Chakravarty <suryash.c@gmail.com>
2026-01-14 16:02:29 -0800

    added test file for get_reg_line

---
+++ tests/unit/test_get_line.py
@@ -0,0 +1,7 @@
+"""
+A test module that tests the ridge_r2 module.
+
+This file contains tests which are used to test the output of the function
+ridge_get_r2(), in the ridge_r2 module. The tests can be run by calling 
+pytest -v . in the root directory
+"""
